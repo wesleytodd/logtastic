de1e816d7b4448294d272b05aac922c05d2d2d58
Wes Todd <wes@wesleytodd.com>
2015-11-13 18:40:35 -0600

    updates for browser compatibility

--- Logger.js
+++ Logger.js
@@ -1,46 +1,10 @@
-var Loggerr = require('loggerr'),
-	cliFormatter = require('loggerr/formatters/cli'),
-	minimist = require('minimist'),
-	util = require('util'),
-	onFinished = require('on-finished'),
-	fs = require('fs');
+var Loggerr = require('loggerr');
+var util = require('util');
+var onFinished = require('on-finished');
 
 var Logtastic = module.exports = function(options) {
 	// Setup default options
 	options = options || {};
-	options.cli = options.cli || Logtastic.defaultOptions.cli;
-	options.parseArgv = typeof options.parseArgv !== 'undefined' ? options.parseArgv : Logtastic.defaultOptions.parseArgv;
-
-	// Parse argv
-	if (options.parseArgv) {
-		// Parse the args
-		var argv = minimist(process.argv, options.parseArgv);
-
-		// Set log level
-		if (argv.silent) {
-			options.level = Logtastic.SILENT;
-		} else if (argv.debug) {
-			options.level = Logtastic.DEBUG;
-		} else if (argv.verbose) {
-			options.level = Logtastic.INFO;
-		} else if (argv.logLevel) {
-			var i = Logtastic.levels.indexOf(argv.logLevel);
-			if (i !== -1) {
-				options.level = i;
-			}
-		}
-
-		// Force log to file
-		if (argv.logToFile) {
-			options.logToFile = true;
-			options.cli = false;
-		}
-	}
-
-	// If cli, specify the cli colored formatter
-	if (options.cli) {
-		options.formatter = cliFormatter;
-	}
 
 	// Call the parent constructor
 	Loggerr.call(this, {
@@ -50,9 +14,8 @@ var Logtastic = module.exports = function(options) {
 	});
 
 	// Setup the properties of the logger
-	this.logToFile = options.logToFile || !options.cli || Logtastic.defaultOptions.logToFile;
-	this.outfile = options.outfile || Logtastic.defaultOptions.outfile;
-	this.errfile = options.errfile || Logtastic.defaultOptions.errfile;
+	this.outfile = options.outfile || 'stdout.log';
+	this.errfile = options.errfile || 'stderr.log';
 
 	// This flag will be set to true when the
 	// file streams are opened for the first time
@@ -68,95 +31,23 @@ Logtastic.levels.forEach(function(level, i) {
 Logtastic.SILENT = -1;
 
 /**
- * The default options
- *
- */
-Logtastic.defaultOptions = {
-	cli: !!process.stdout.isTTY,
-	logToFile: false,
-	outfile: 'stdout.log',
-	errfile: 'stderr.log',
-	parseArgv: {
-		string: [
-			'logLevel'
-		],
-		boolean: [
-			'logToFile',
-			'debug',
-			'verbose',
-			'silent'
-		],
-		alias: {
-			f: 'logToFile',
-			l: 'logLevel',
-			v: 'verbose',
-			s: 'silent'
-		}
-	},
-	middleware: {
-		level: Logtastic.INFO,
-		immediate: false
-	}
-};
-
-/**
  * Override log so we can open our filestreams
- *
  */
 Logtastic.prototype.log = function(level, msg, extra, done) {
-	if (this.logToFile && !this._fileStreamsOpen) {
+	if (this._openFileStreams && this._fileStreamsOpen) {
+		this._fileStreamsOpen = true;
 		this._openFileStreams();
 	}
 	return Loggerr.prototype.log.call(this, level, msg, extra, done);
 };
 
 /**
- * Initalize the streams
- *
- */
-Logtastic.prototype._openFileStreams = function() {
-	// Only open the files once
-	if (this._fileStreamsOpen) {
-		return;
-	}
-
-	this._fileStreamsOpen = true;
-	var stdout = fs.createWriteStream(this.outfile, {flags: 'a'});
-	var stderr = fs.createWriteStream(this.errfile, {flags: 'a'});
-	this.streams = Logtastic.levels.map(function(l, i) {
-		return i > Logtastic.WARNING ? stdout : stderr;
-	});
-};
-
-/**
- * Start logging uncaught exceptions
- *
- */
-Logtastic.prototype.logUncaught = function(level) {
-	// Do we have that level?
-	var logFnc = this[Logtastic.levels[level || Logtastic.EMERGENCY]];
-	if (!logFnc) {
-		return;
-	}
-
-	// Bind fnc to this and add listener
-	logFnc = logFnc.bind(this);
-	process.on('uncaughtException', logFnc);
-
-	// Return an off function
-	return function() {
-		process.removeListener('uncaughtException', logFnc);
-	};
-};
-
-/**
  * An express style middleware function for logging requests
- *
  */
 Logtastic.prototype.middleware = function(options) {
 	options = options || {};
-	options.immediate = options.immediate || Logtastic.defaultOptions.middleware.immediate;
-	options.level = options.level || Logtastic.defaultOptions.middleware.level;
+	options.immediate = options.immediate || false;
+	options.level = options.level || Logtastic.INFO;
 
 	var doLog = function(req, res) {
 		var msg = [
@@ -183,5 +74,25 @@ Logtastic.prototype.middleware = function(options) {
 			});
 		}
 		next();
+	};
+};
+
+/**
+ * Start logging uncaught exceptions
+ */
+Logtastic.prototype.logUncaught = function(level) {
+	// Do we have that level?
+	var logFnc = this[Logtastic.levels[level || Logtastic.EMERGENCY]];
+	if (!logFnc) {
+		return;
+	}
+
+	// Bind fnc to this and add listener
+	logFnc = logFnc.bind(this);
+	process.on('uncaughtException', logFnc);
+
+	// Return an off function
+	return function() {
+		process.removeListener('uncaughtException', logFnc);
 	};
 };
--- index.js
+++ index.js
@@ -1,8 +1,66 @@
+var minimist = require('minimist');
 var Logtastic = require('./Logger');
+
+var options = {};
+
+// Parse argv
+var argv = minimist(process.argv, {
+	string: [
+		'logLevel'
+	],
+	boolean: [
+		'logToFile',
+		'debug',
+		'verbose',
+		'silent'
+	],
+	alias: {
+		f: 'logToFile',
+		l: 'logLevel',
+		v: 'verbose',
+		s: 'silent'
+	}
+});
+
+// Set log level
+if (argv.silent) {
+	options.level = Logtastic.SILENT;
+} else if (argv.debug) {
+	options.level = Logtastic.DEBUG;
+} else if (argv.verbose) {
+	options.level = Logtastic.INFO;
+} else if (argv.logLevel) {
+	var i = Logtastic.levels.indexOf(argv.logLevel);
+	if (i !== -1) {
+		options.level = i;
+	}
+}
+
+// If cli, specify the cli colored formatter
+if (!argv.logToFile && !!process.stdout.isTTY) {
+	options.formatter = require('loggerr/formatters/cli');
+}
 
 // Export an instance of logger
 // with the default options
-var log = module.exports = new Logtastic();
+var log = module.exports = new Logtastic(options);
+
+// If logging to file add open streams method
+if (argv.logToFile) {
+	log._openFileStreams = function() {
+		// Only open the files once
+		if (log._fileStreamsOpen) {
+			return;
+		}
+
+		log._fileStreamsOpen = true;
+		var stdout = fs.createWriteStream(log.outfile, {flags: 'a'});
+		var stderr = fs.createWriteStream(log.errfile, {flags: 'a'});
+		log.streams = Logtastic.levels.map(function(l, i) {
+			return i > Logtastic.WARNING ? stdout : stderr;
+		});
+	};
+}
 
 // Export the constructor function
 log.Logtastic = Logtastic;
